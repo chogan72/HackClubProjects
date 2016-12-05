44b87e8851244bdcef7cf371fe56281d0f1f1d7e
Chris Hogan <chrishogan99@hotmail.com>
2016-12-05 21:44:29 +0000

    Add existing file

--- dodge/game.js
+++ dodge/game.js
@@ -6,14 +6,14 @@ var enemyImage;
 var backgroundImage;
 
 function preload() {
-  playerImage = loadImage("https://surrogate.hackedu.us/i.imgur.com/N5uCbDu.png");
-  enemyImage = loadImage("https://surrogate.hackedu.us/i.imgur.com/OdL0XPt.png");
-  backgroundImage = loadImage("https://surrogate.hackedu.us/i.imgur.com/aKQOg3G.png");
+  playerImage = loadImage("http://cdn.bleacherreport.net/images/team_logos/64x64/arizona_cardinals.png");
+  enemyImage = loadImage("http://cdn.bleacherreport.net/images/team_logos/64x64/seattle_seahawks.png");
+  backgroundImage = loadImage("http://a.espncdn.com/photo/2008/0730/phoenix_580.jpg");
 }
 
 function setup() {
   isGameOver = false;
-  createCanvas(256, 256);
+  createCanvas(580, 326);
   player = createSprite(width / 2, height - (playerImage.height / 2), 0, 0);
   player.addImage(playerImage);
   enemy = createSprite(width / 2, 0, 0, 0);
@@ -33,15 +33,15 @@ function draw() {
     }
     background(backgroundImage);
     if (keyDown(RIGHT_ARROW) && player.position.x < (width - (playerImage.width / 2))) {
-      player.position.x += 2;
+      player.position.x += 3;
     }
     if (keyDown(LEFT_ARROW) && player.position.x > (playerImage.width / 2)) {
-      player.position.x -= 2;
+      player.position.x -= 3;
     }
     enemy.position.y = enemy.position.y + 3;
     if (enemy.position.y > height) {
       enemy.position.y = 0;
-      enemy.position.x = random(5, width - 5);
+      enemy.position.x = random(5, width - 3);
     }
     drawSprites();
   }
