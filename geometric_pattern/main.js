34c16deb0c3946400366c82dec37b43c7751987a
Chris Hogan <chrishogan99@hotmail.com>
2016-12-09 02:52:37 +0000

    Added Jarring animation to geometric_pattern

--- geometric_pattern/main.js
+++ geometric_pattern/main.js
@@ -1,25 +1,30 @@
 var NUM_CIRCLES = 12;
+
 var circleDiameter;
-var circleradius;
+var circleRadius;
+
 var rVal;
 var gVal;
 var bVal;
 
 function setup() {
-    createCanvas(480,600);
-    circleDiameter = width/NUM_CIRCLES;
-    circleRadius = circleDiameter/2;
+  createCanvas(480, 600);
+
+  frameRate(5);
+
+  circleDiameter = width/NUM_CIRCLES;
+  circleRadius = circleDiameter/2;
+
+  rVal = 255;
+  gVal = 0;
+  bVal = 0;
 }
 
 function draw() {
-    rVal = 255;
-    gVal = 0;
-    bVal = 0;
-  
-    var isShifted = false;
+  var isShifted = false;
 
-    var y = height;
-    while (y >= 0) {
+  var y = height;
+  while (y >= 0) {
 
     var x;
 
@@ -28,27 +33,19 @@ function draw() {
     } else {
       x = 0;
     }
-    
-    fill(color(rVal,gVal,bVal));
-    stroke(color(rVal,gVal,bVal));
-    
+
     while (x <= width) {
+      stroke(color(rVal, gVal, bVal));
+      fill(color(rVal, gVal, bVal));
       ellipse(x, y, circleDiameter, circleDiameter);
       x = x + circleDiameter;
     }
 
     y = y - circleRadius;
     isShifted = !isShifted;
-    
-    rVal = rVal - 2;
-    gVal = gVal + 7;
-    bVal = bVal + 3;
-  }
-}
 
-function keyPressed() {
-  if (keyCode === 115 || keyCode === 83) {
-    saveCanvas('geometricPattern', 'png');
+    rVal = (rVal + 254) % 256;
+    gVal = (gVal + 7) % 256;
+    bVal = (bVal + 3) % 256;
   }
-  return false;
 }
